1996359a91181af994a929e7d5064f6b9b4ca774
RowanSe7en <bilal.rouane702@gmail.com>
2026-03-27 17:18:53 +0100

    i'll save my love for you

--- python06/alchemy/grimoire/validator.py
+++ python06/alchemy/grimoire/validator.py
@@ -1,7 +1,7 @@
 def validate_ingredients(ingredients: str) -> str:
 
-    valid_elements = ["fire", "water", "earth", "air"]
-    words = ingredients.split()
+    valid_elements: list[str] = ["fire", "water", "earth", "air"]
+    words: list[str] = ingredients.split()
     
     for word in words:
         if word in valid_elements:
--- python06/alchemy/potions.py
+++ python06/alchemy/potions.py
@@ -1,17 +1,17 @@
 from .elements import create_fire, create_water, create_earth, create_air
 
-fire = create_fire()
-water = create_water()
-def healing_potion():
+fire: str = create_fire()
+water: str = create_water()
+def healing_potion() -> str:
     return f"Healing potion brewed with {fire} and {water}"
 
-earth = create_earth()
-def strength_potion():
+earth: str = create_earth()
+def strength_potion() -> str:
     return f"Strength potion brewed with {earth} and {fire}"
 
-air = create_air()
-def invisibility_potion():
+air: str = create_air()
+def invisibility_potion() -> str:
     return f"Invisibility potion brewed with {air} and {water}"
 
-def wisdom_potion():
+def wisdom_potion() -> str:
     return f"Wisdom potion brewed with all elements: {fire}, {water}, {earth}, {air}"
